8f364e4a0e4a559562d19e84457a470a8b2748a0
Amer <amerwwww360@gmail.com>
2026-08-25 21:11:20 +0300

    17- Showing Mutation Progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -11,16 +11,12 @@ const TodoForm = () => {
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
     onSuccess: (savedTodo, newTodo) => {
-      // Approach: Invalidate the cache
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"],
-      // });
-      //
-      // Approach 2: Updating the data in the chach
       queryClient.setQueriesData<Todo[]>(["todos"], (todos) => [
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -47,7 +43,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button disabled={addTodo.isLoading} className="btn btn-primary">
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
