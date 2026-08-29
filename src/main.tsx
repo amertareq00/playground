7bf34a985ab3aae7fbf648c542943aed4a57ee78
Amer <amerwwww360@gmail.com>
2026-08-29 19:57:34 +0300

    2- Setting Up Routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,26 +5,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes";
 
-// Just to know that we can configur what ever we want here
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       retry: 3,
-//       cacheTime: 300_000, // 5m
-//       staleTime: 10 * 1000, // 10s
-//       refetchOnWindowFocus: false,
-//       refetchOnReconnect: false,
-//       refetchOnMount: false,
-//     },
-//   },
-// });
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
